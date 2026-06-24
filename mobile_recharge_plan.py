ef777a83275e0e1bffb593fd95fc79834a45fe5a
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-25 01:10:24 +0530

    add mobile recharge plan program

---
+++ mobile_recharge_plan.py
@@ -0,0 +1,8 @@
+# Mobile Recharge Plan Program
+
+cost_per_gb = float(input("Enter the cost per GB: "))
+number_of_gb = float(input("Enter the number of GBs: "))
+
+total_recharge = cost_per_gb * number_of_gb
+
+print("Total Recharge Cost :", total_recharge)
