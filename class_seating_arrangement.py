269915ffd50d7c6f96ab85bf563cd4fa4cab4236
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-24 22:41:25 +0530

    add class seating arrangement program

---
+++ class_seating_arrangement.py
@@ -0,0 +1,4 @@
+total_students =int(input("Enter total students :"))
+students_per_row = int(input("Enter students per row :"))
+complete_rows = total_students//students_per_row
+print("Number of complete Rows : ",complete_rows)
