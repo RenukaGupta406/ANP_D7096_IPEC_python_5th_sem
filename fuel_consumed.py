6a9afdf2dbf4710483a1444e69cf2fb7c0b31bcd
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-24 22:03:14 +0530

    add fuel consumed program

---
+++ fuel_consumed.py
@@ -0,0 +1,4 @@
+distance = float(input("Enter the distance travelled :"))
+fuel_consm =float(input("Enter the fuel (in litres) :"))
+mileage =distance/fuel_consm
+print("The average mieage of car is :",mileage)
