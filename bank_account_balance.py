a3d238dcb67390977ec2bd24dbb867f9780515a2
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-24 22:26:30 +0530

    add bank account balance program

---
+++ bank_account_balance.py
@@ -0,0 +1,4 @@
+current_balance =float(input("Enter the current balance of your account :"))
+withdrawal_amount = float(input("Enter the amount you withdrawal :"))
+remain_balance = current_balance-withdrawal_amount
+print("the remaining balance of yur account :",remain_balance)
