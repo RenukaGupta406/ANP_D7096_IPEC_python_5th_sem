b08786ec668f52d158519a11a238d2e2ce49a76d
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-24 21:49:50 +0530

    add total price of items program

---
+++ total_price_of_items.py
@@ -0,0 +1,4 @@
+price= float(input("Enter he price of the item you purchase :"))
+num = int(input("Enter the number of items :"))
+total_price =price*num
+print("Total price of the item is :",total_price)
