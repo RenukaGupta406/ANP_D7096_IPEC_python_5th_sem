5d4b68ca3269ba940a1576c680aff0a908be3f2a
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-25 01:16:09 +0530

    add event management budget program

---
+++ event_management_budget.py
@@ -0,0 +1,8 @@
+# Event Management Budget Program
+
+total_event_cost = float(input("Enter the total event cost: "))
+number_of_participants = int(input("Enter the number of participants: "))
+
+amount_per_participant = total_event_cost / number_of_participants
+
+print("Amount per Participant :", amount_per_participant)
