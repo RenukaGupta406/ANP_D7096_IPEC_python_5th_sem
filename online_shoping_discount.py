3e84c4102bc86aa8087f7df5d3998861484f4e74
RenukaGupta406 <renukagupta006@gmail.com>
2026-06-25 01:01:01 +0530

    add online shopping discount program

---
+++ online_shoping_discount.py
@@ -0,0 +1,8 @@
+# Online Shopping Discount Program
+
+product_price = float(input("Enter the product price: "))
+discount_amount = float(input("Enter the discount amount: "))
+
+final_price = product_price - discount_amount
+
+print("Final payable amount :", final_price)
